5ada26c3365bb29f381269773d6751dcab92cfbc
Hafiffdl <hafif.fdl16@gmail.com>
2023-10-27 18:07:43 +0700

    update UI

--- katalog_elektronik/lib/screens/beranda/home_screen_viewmodel.dart
+++ katalog_elektronik/lib/screens/beranda/home_screen_viewmodel.dart
@@ -11,13 +11,13 @@ class HomeScreenViewModel extends ChangeNotifier {
       price: 2300000.00,
     ),
     Product(
-      name: 'Samsung 43 Inci Qled',
+      name: 'Samsung Qled',
       description: 'Dengan fitur 100% Color Volume with Quantum Dot yang dapat mengambil cahaya dan mengubahnya menjadi warna menakjubkan menjadikan Samsung QLED 4K Smart TV 43 inci QA43Q60BAKXXD ini mampu memberikan tontonan yang terasa nyata pada tingkat kecerahan apa pun. Dilengkapi Quantum HDR membuat Smart TV ini juga mampu menghadirkan detail dan kontras untuk dapat merasakan kekuatan penuh di setiap gambar. Selain itu, didukung prosesor Quantum Processor Lite 4K untuk mengoptimalkan kualitas suara konten yang Anda tonton. Teknologi 4K Upscaling juga memastikan Anda mendapatkan resolusi hingga 4K untuk konten yang Anda sukai.',
       imageUrl: 'assets/tvsamsung.png',
       price: 9400000.00,
     ),
     Product(
-      name: 'Polytron 450 Ltr Kulkas',
+      name: 'Polytron Kulkas',
       description: 'Lemari es persembahan Polytron ini dilengkapi Inverter Technology yang membuatnya memiliki kinerja maksimal dengan lebih hemat energi hingga 20%. Hadir dengan desain premium yang dilengkapi LCD Display untuk melakukan pengaturan suhu secara digital dengan penampilan layar LCD yang modern menjadikan lemari es ini mampu menciptakan kesan mewah pada dapur dan ruang makan Anda. Selain itu, lemari es Polytron PRS 480X juga sudah dilengkapi dispenser air dengan kapasitas dispenser air dingin hingga 2 liter. Tidak perlu khawatir akan suara bising yang mengganggu, dalam melakukan kinerjanya lemari es ini memiliki tingkat kebisingan Ultra Silent.',
       imageUrl: 'assets/kulkaspolytron.png',
       price: 9600000.00,
@@ -65,13 +65,13 @@ class HomeScreenViewModel extends ChangeNotifier {
       price: 6650000.00,
     ),
     Product(
-      name: 'Apple Watch Trail Loop',
+      name: 'Apple Watch with Trail Loop',
       description: 'Apple Watch paling tangguh dan andal yang pernah ada, dirancang untuk penjelajahan, petualangan, dan ketahanan. Dengan casing titanium 49 mm sekelas industri dirgantara, kekuatan baterai yang lebih lama,1 aplikasi khusus yang berfungsi dengan sensor canggih, dan tombol Tindakan baru yang dapat disesuaikan.',
       imageUrl: 'assets/applewatch.png',
       price: 13999000.00,
     ),
     Product(
-      name: 'Apple Watch Series 8',
+      name: 'Apple Watch Series 8 midnight',
       description: 'Apple Watch Series 8 dilengkapi aplikasi dan sensor kesehatan canggih, sehingga Anda dapat melakukan pengukuran EKG,1 detak jantung, dan kadar oksigen darah,2 serta memantau perubahan suhu3 untuk informasi mendalam tentang siklus menstruasi Anda.4 Dan dengan Deteksi Tabrakan, pemantauan tahap tidur, serta metrik olahraga canggih, Apple Watch Series 8 membantu Anda tetap aktif, sehat, selamat, dan terhubung.',
       imageUrl: 'assets/applewatch8.png',
       price: 6999000.00,
