4079afd68bc224749172049621e8aa8a237951df
Hafiffdl <hafif.fdl16@gmail.com>
2023-11-14 13:38:32 +0700

    revisi add: UI Testing news

---
+++ katalog_elektronik/test/news_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Terdapat Text "News" pada NewsScreen', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: Scaffold(
+          body: Text('News'),
+        ),
+      ),
+    );
+    expect(find.text("News"), findsOneWidget);
+  });
+}
